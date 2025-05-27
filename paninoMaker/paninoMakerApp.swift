8596b89b6952b63204deb58bb456e80282aa24d8
alex mazzoni <alexmaz03@hotmail.it>
2025-05-27 11:48:25 +0200

    Initial Commit

---
+++ paninoMaker/paninoMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  paninoMakerApp.swift
+//  paninoMaker
+//
+//  Created by alex mazzoni on 27/05/25.
+//
+
+import SwiftUI
+
+@main
+struct paninoMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ paninoMakerTests/paninoMakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  paninoMakerTests.swift
+//  paninoMakerTests
+//
+//  Created by alex mazzoni on 27/05/25.
+//
+
+import Testing
+@testable import paninoMaker
+
+struct paninoMakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
