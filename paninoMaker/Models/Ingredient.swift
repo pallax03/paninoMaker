fdf9feb90c41f6ae358385beb9fc17b20ccebdb6
alex mazzoni <alexmaz03@hotmail.it>
2025-05-27 11:53:40 +0200

    init project

---
+++ paninoMaker/Models/Ingredient.swift
@@ -0,0 +1,8 @@
+//
+//  Ingredient.swift
+//  paninoMaker
+//
+//  Created by alex mazzoni on 27/05/25.
+//
+
+import Foundation
---
+++ paninoMaker/Models/Panino.swift
@@ -0,0 +1,8 @@
+//
+//  Panino.swift
+//  paninoMaker
+//
+//  Created by alex mazzoni on 27/05/25.
+//
+
+import Foundation
